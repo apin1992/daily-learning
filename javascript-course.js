3acdd8111383b5a302c9deff7c42f5f90898a8b8
Martin <martin@Martins-MacBook-Pro.local>
2026-04-18 09:22:17 -0700

    added to javascript course

---
+++ javascript-course.js
@@ -0,0 +1,36 @@
+// // // this is a comment
+
+const user = {
+    name: "Boba",
+    age: 40,
+    height: 1.65, // meters
+    weight: 90 // kilos
+};
+
+console.log(user.height);
+
+
+//const numbers = [1, 2, 3];
+
+//numbers.forEach(n => console.log(n));
+
+// const fruits = ["apple", "orange", "bannana"];
+// fruits.forEach(f => console.log(f));
+
+// const greet = (name) => "Hello " + name;
+// console.log(greet("Boba"));
+
+// function greet(name) {
+//     return "Hello " + name;
+// }
+
+// console.log(greet("Boba"));
+
+
+// let age = 10;
+
+// if (age > 18) {
+//     console.log("adult");
+// } else {
+//     console.log("minor");
+// }
